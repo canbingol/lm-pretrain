add4ea1d51b5d07739db8a6d88795b5e15b39768
Can <canbingol36@gmail.com>
2025-09-17 00:16:14 +0300

    added eos and bos token

--- data_loader.py
+++ data_loader.py
@@ -2,12 +2,14 @@ import os
 import torch, gc
 from itertools import islice
 from torch.utils.data import Dataset, DataLoader
-from datasets import load_dataset,get_dataset_config_info
+from datasets import load_dataset
 from tqdm import tqdm
 
-def prepare_train_data(hf_data_name,tokenizer,output_path,batch_size,context_len=512,len_data_row=4_000,val_ratio=0.06):
+def prepare_train_data(hf_data_name,tokenizer,output_path,batch_size,context_len=512,len_data_row=1_000,val_ratio=0.06):
     sample_file = f"{output_path}/sample.txt"
     dataset = load_dataset(hf_data_name, split="train", streaming=True)
+    dataset = dataset.with_format("torch")
+
     print("tokenizing...")
 
     train_tokens, val_tokens = [], []
@@ -29,11 +31,13 @@ def prepare_train_data(hf_data_name,tokenizer,output_path,batch_size,context_len
         buf.clear()
     print(f"{total_tokens} token tokenized")
     class TextDataset(Dataset):
-        def __init__(self, tokens, max_length=512, stride=512):
+        def __init__(self, tokens, max_length=512, stride=512,eos_token=3,bos_token=2):
             self.input_ids, self.target_ids = [], []
             for i in tqdm(range(0, len(tokens) - max_length, stride), desc="building chunks"):
+                tokens = [bos_token] + tokens
+
                 inp = tokens[i:i + max_length]
-                tgt = tokens[i + 1:i + max_length + 1]
+                tgt = tokens[i + 1:i + max_length] + [eos_token]
                 self.input_ids.append(torch.tensor(inp))
                 self.target_ids.append(torch.tensor(tgt))
 
@@ -46,9 +50,9 @@ def prepare_train_data(hf_data_name,tokenizer,output_path,batch_size,context_len
     train_dataset = TextDataset(train_tokens, max_length=context_len, stride=context_len)
     val_dataset   = TextDataset(val_tokens,   max_length=context_len, stride=context_len)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False,
-                            num_workers=2, pin_memory=True)
+                            num_workers=0, pin_memory=True)
     val_loader   = DataLoader(val_dataset,   batch_size=batch_size, shuffle=False,
-                            num_workers=2, pin_memory=True)
+                            num_workers=0, pin_memory=True)
 
     with open(sample_file, "w", encoding="utf-8") as f:
         for i in range(2):
